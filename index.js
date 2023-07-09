663d5dfbe28976a4b2561cf8c65a4d6c3c827269
stevenhiltrop <stevenhiltrop@gmail.com>
2023-07-09 16:23:47 +1200

    Added functioning code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// Grab website and make iframe
 let newssite = document.getElementById('newssite').src = document.getElementById("newsinput").value;
+
+//Remove all parameter element attributes that put up paywall
+const allParameters = document.querySelectorAll('p');
+
+allParameters.forEach((parameter) => {
+    parameter.removeAttribute('class');
+    parameter.removeAttribute('style');
+});
+
